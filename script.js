62114c47235a5b04b8434a129d3fd0af80cdec38
Martine Dowden <martine@andromeda16.com>
2022-03-03 13:29:39 -0500

    seperate concerns

---
+++ script.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  let currentIndex = 0;
+  const phrases = [
+    'You Rock!',
+    'You Got This!',
+    'You Are Going to Rock This',
+    'You da Best',
+    'Go get\'em Champ!',
+    'I\'m so proud of you',
+    'You can do this!',
+    'You\'re fantastic!',
+    'You are The Machine'
+  ]
+
+  var button = document.getElementById('validateMe');
+  var phraseContainer = document.getElementById('phraseContainer');
+
+  button.onclick = function() {
+    phraseContainer.innerHTML = '';
+    let randomNumber = Math.floor(Math.random() * Math.floor(phrases.length-1));
+    while (randomNumber === currentIndex) {
+      randomNumber = Math.floor(Math.random() * Math.floor(phrases.length-1));
+    }
+    phraseContainer.innerHTML = phrases[randomNumber];
+
+    currentIndex = randomNumber
+  }
+})();
